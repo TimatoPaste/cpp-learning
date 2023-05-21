e91a2eab0dc9d62873946aabce123268f19012e9
TimatoPaste <70876288+TimatoPaste@users.noreply.github.com>
2023-05-21 01:14:18 -0700

    added library SDL

--- 007 Graphics Window/main.cpp
+++ 007 Graphics Window/main.cpp
@@ -1,3 +1,9 @@
 #include <iostream>
 #include <string>
-#include <graphics.h>
+#include "SDL2-2.26.5/include/SDL.h"
+using namespace std;
+//why isn't SDL working?
+int main(){
+	cout<<"hi";
+	return 0;
+}
